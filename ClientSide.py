ec78b12386ad37ab82f685672912b376635eeae4
tal_bar_or <tbaror@gmail.com>
2015-09-04 20:18:14 +0300

    update trace ctrl

---
+++ ClientSide.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# skeleton from http://kmkeen.com/socketserver/2009-04-03-13-45-57-003.html
+
+import socketserver, subprocess, sys
+from threading import Thread
+from pprint import pprint
+import json
+
+my_unix_command = ['bc']
+HOST = '0.0.0.0'
+PORT = 2000
+
+class SingleTCPHandler(socketserver.BaseRequestHandler):
+    "One instance per connection.  Override handle(self) to customize action."
+    def handle(self):
+        # self.request is the client connection
+        data = self.request.recv(1024)  # clip input at 1Kb
+        text = data.decode('utf-8')
+        pprint(json.loads(text))
+        self.request.send('OK'.encode('utf-8'))
+        self.request.close()
+
+class SimpleServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    # Ctrl-C will cleanly kill all spawned threads
+    daemon_threads = True
+    # much faster rebinding
+    allow_reuse_address = True
+
+    def __init__(self, server_address, RequestHandlerClass):
+        socketserver.TCPServer.__init__(self, server_address, RequestHandlerClass)
+
+if __name__ == "__main__":
+    client = SingleTCPHandler()
+    # terminate with Ctrl-C
+    
