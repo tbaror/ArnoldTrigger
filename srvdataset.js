5f7660349cdb8d04b9a2c2047b2a506167d452e1
talbaror <tbaror@gmail.com>
2015-10-10 21:31:29 +0300

    event query fixed

--- srvdataset.js
+++ srvdataset.js
@@ -1,12 +1,7 @@
 {"ArnoldSite":[
-    {"EnforcementHost":"127.0.0.1",
+    {"EnforcementHost":"0.0.0.0",
     "SrvPort":"3031",
-    "EventID":"1001",
-    "EventIdII":"4097",
-    "LogType":"Application",
-    "EvtSourceName":"Windows Error Reporting",
-    "EvtSourceNameII":"Microsoft-Windows-CAPI2",
-    "PassFile":"tokenpass",
-    "NumEvt":"1"
+    "QuarantineIp":"169.254.1.220",
+    "QrnPort":"80"
     }
 ]}
