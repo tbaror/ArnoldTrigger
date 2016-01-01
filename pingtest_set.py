750323e9f86acf8376f36a2d0dc4bce79646293f
tbaror <tbaror@dalet.com>
2016-01-01 12:06:00 +0200

    server progress skelaton works

---
+++ pingtest_set.py
@@ -0,0 +1,26 @@
+# Import modules
+import subprocess
+import ipaddress
+
+
+
+# Configure subprocess to hide the console window
+info = subprocess.STARTUPINFO()
+info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+info.wShowWindow = subprocess.SW_HIDE
+
+# For each IP address in the subnet, 
+# run the ping command with subprocess.popen interface
+statusa = 0
+
+while statusa == 0:
+    output = subprocess.Popen(['ping', '-n', '1', '-w', '500', '212.143.237.31'], stdout=subprocess.PIPE, startupinfo=info).communicate()[0]
+    
+    if "Destination host unreachable" in output.decode('utf-8'):
+        print('212.143.237.31', "is Offline")
+    elif "Request timed out" in output.decode('utf-8'):
+        print('212.143.237.31', "is Offline")
+    else:
+
+        print('212.143.237.31', "is Online")
+        statusa = 1
