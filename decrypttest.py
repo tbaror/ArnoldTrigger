2b233ff2a874d7bd7865b881126b2eec7e3b2783
Tal Bar-Or <tal_baror@hotmail.com>
2016-01-12 13:25:57 +0200

    Delete decrypttest.py

--- decrypttest.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import base64
-
-from cryptography.fernet import Fernet
-
-f = Fernet(b'tIoUIXthxKcwzYY9uEKnyyir_eKQUlCqvB1tKH5eG7U=')
-b= bytes('tIoUIXthxKcwzYY9uEKnyyir_eKQUlCqvB1tKH5eG7U=','utf-8')
-print(b)
