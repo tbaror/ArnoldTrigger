ad9079a7a8c337341b6f2e8eb35383beed1ec445
Tal Bar-Or <tal_baror@hotmail.com>
2016-01-12 13:29:42 +0200

    Delete unziptest.py

--- unziptest.py
+++
@@ -1,6 +0,0 @@
-import zipfile
-fh = open("c:/force10-s4810_User's Guide5_en-us.zip", 'rb')
-z = zipfile.ZipFile(fh)
-for name in z.namelist():
-    outpath = "C:/testzip"
-    z.extract(name, outpath)
